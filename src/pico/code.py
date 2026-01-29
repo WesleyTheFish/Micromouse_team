1b3e15c93e815d87c82e75aa77d5940e12cda53f
Miriam Sinton-Remes <miriamsintonremes@gmail.com>
2026-01-29 17:04:53 -0500

    Added a systems check to code.py

--- src/pico/code.py
+++ src/pico/code.py
@@ -5,15 +5,19 @@ import assembly
 def main():
     mouse = assembly.Assembly()
 
-
-    mouse.move_forward()
-    time.sleep(2)
-    mouse.stop_motors()
-    time.sleep(1)
-    mouse.move_backward()
-    time.sleep(2)
-    mouse.stop_motors()
-
+    while True:
+        mouse.move_forward()
+        time.sleep(2)
+        mouse.stop_motors()
+        time.sleep(1)
+        mouse.move_backward()
+        time.sleep(2)
+        mouse.stop_motors()
+        print("IMU gyro: " + mouse.imu.getGyroscope())
+        print("Left encoder: " + mouse.left_encoder.read())
+        print("Right encoder: " + mouse.right_encoder.read())
+        print("Distance sensor: " + mouse.distance.get_distance())
+        time.sleep(1)
 
 
 if __name__ == "__main__":
