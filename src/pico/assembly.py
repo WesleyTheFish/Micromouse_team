b724afbdea50327010fb197cb45ddf3fe42b8f32
Miriam Sinton-Remes <miriamsintonremes@gmail.com>
2026-03-31 19:07:01 -0400

    Updated Motor Ports

--- src/pico/assembly.py
+++ src/pico/assembly.py
@@ -11,15 +11,15 @@ COUNTERCLOCKWISE = (False, True)  # BACKWARD
 class Assembly:
     def __init__(self):
         # Motors
-        self.motor_right = motor.Motor(board.GP2, board.GP3, board.GP6)
-        self.motor_left = motor.Motor(board.GP4, board.GP5, board.GP7)
+        self.motor_right = motor.Motor(board.GP16, board.GP17, board.GP18)
+        self.motor_left = motor.Motor(board.GP15, board.GP14, board.GP13)
 
         # Encoders
-        self.right_encoder = encoder.Encoder(board.GP18, board.GP19)
-        self.left_encoder = encoder.Encoder(board.GP12, board.GP13)
+        self.right_encoder = encoder.Encoder(board.GP8, board.GP19)
+        self.left_encoder = encoder.Encoder(board.GP12, board.GP3)
 
         # IMU
-        self.imu = imu.IMU(board.GP14, board.GP15)
+        self.imu = imu.IMU(board.GP4, board.GP5)
 
         # Distance Sensor
         self.distance = distance_sensor.DistanceSensor(board.GP10, board.GP11)
