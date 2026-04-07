1474d7543932c0baa1b2fc99a5c6cfb8ed4820a4
Miriam Sinton-Remes <miriamsintonremes@gmail.com>
2026-04-07 19:35:14 -0400

    Made motor speed adjustable

--- src/pico/assembly.py
+++ src/pico/assembly.py
@@ -1,4 +1,5 @@
 import board # type: ignore
+import digitalio
 import motor
 import encoder
 import imu
@@ -7,6 +8,7 @@ import distance_sensor
 # Define directions
 CLOCKWISE = (True, False)  # FORWARD
 COUNTERCLOCKWISE = (False, True)  # BACKWARD
+DEFAULT_POWER = 65000
 
 class Assembly:
     def __init__(self):
@@ -19,29 +21,29 @@ class Assembly:
         self.left_encoder = encoder.Encoder(board.GP12, board.GP3)
 
         # IMU
-        self.imu = imu.IMU(board.GP4, board.GP5)
+        # self.imu = imu.IMU(board.GP4, board.GP5)
 
         # Distance Sensor
-        self.distance = distance_sensor.DistanceSensor(board.GP10, board.GP11)
+        # self.distance = distance_sensor.DistanceSensor(board.GP10, board.GP11)
 
 
 
-    def move_forward(self):
-        self.motor_right.move_forward(CLOCKWISE)
-        self.motor_left.move_forward(CLOCKWISE)
+    def move_forward(self, speed=DEFAULT_POWER):
+        self.motor_right.move_forward(CLOCKWISE, speed)
+        self.motor_left.move_forward(CLOCKWISE, speed)
 
-    def move_backward(self):
-        self.motor_right.move_backward(COUNTERCLOCKWISE)
-        self.motor_left.move_backward(COUNTERCLOCKWISE)
+    def move_backward(self, speed=DEFAULT_POWER):
+        self.motor_right.move_backward(COUNTERCLOCKWISE, speed)
+        self.motor_left.move_backward(COUNTERCLOCKWISE, speed)
 
     def stop_motors(self):
         self.motor_right.stop()
         self.motor_left.stop()
 
-    def turn_right(self):
-        self.motor_right.move_backward(COUNTERCLOCKWISE)
-        self.motor_left.move_forward(CLOCKWISE)
+    def turn_right(self, speed=DEFAULT_POWER):
+        self.motor_right.move_backward(COUNTERCLOCKWISE, speed)
+        self.motor_left.move_forward(CLOCKWISE, speed)
 
-    def turn_left(self):
-        self.motor_right.move_forward(CLOCKWISE)
-        self.motor_left.move_backward(COUNTERCLOCKWISE)
+    def turn_left(self, speed=DEFAULT_POWER):
+        self.motor_right.move_forward(CLOCKWISE, speed)
+        self.motor_left.move_backward(COUNTERCLOCKWISE, speed)
--- src/pico/code.py
+++ src/pico/code.py
@@ -2,23 +2,28 @@ import time
 import assembly
 import asyncio
 
-async def main():
-    asyncio.create_task(mouse.left_encoder.monitor())
-    asyncio.create_task(mouse.right_encoder.monitor())
+async def control():
     while True:
-        mouse.move_forward()
-        time.sleep(2)
+        mouse.move_forward(10000)
+        await asyncio.sleep(2)
         mouse.stop_motors()
-        time.sleep(1)
-        mouse.move_backward()
-        time.sleep(2)
+        await asyncio.sleep(1)
+        mouse.move_forward(40000)
+        await asyncio.sleep(2)
         mouse.stop_motors()
-        print("IMU gyro: " + mouse.imu.getGyroscope())
-        print("Left encoder: " + mouse.left_encoder.read())
-        print("Right encoder: " + mouse.right_encoder.read())
-        print("Distance sensor: " + mouse.distance.get_distance())
-        time.sleep(1)
+        # print("IMU gyro: " + mouse.imu.getGyroscope())
+        print(f"Left encoder: {mouse.left_encoder.read()}")
+        print(f"Right encoder: {mouse.right_encoder.read()}")
+        # print("Distance sensor: " + mouse.distance.get_distance())
+        await asyncio.sleep(1)
 
+async def main():
+    left_encoder = asyncio.create_task(mouse.left_encoder.monitor())
+    right_encoder = asyncio.create_task(mouse.right_encoder.monitor())
+    # give encoder tasks a moment to start and sample initial states
+    await asyncio.sleep(0.01)
+    primary = asyncio.create_task(control())
+    await asyncio.gather(left_encoder, right_encoder, primary)
 
 if __name__ == "__main__":
     mouse = assembly.Assembly()
